8026e6ceca2e57b0e578f4776ad0a075a5590bcd
Meng <meng.lei086@gmail.com>
2017-03-12 22:20:26 +1100

    final

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -153,7 +153,14 @@ def loops_4():
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ]
     """
-    pass
+    numberfield = []
+    for i in range(10):
+        number = []
+        for j in range(10):
+            number.append(str(j))
+            j += 1
+        numberfield.append(number)
+    return numberfield
 
 
 def loops_5():
@@ -178,7 +185,13 @@ def loops_5():
         "There are {} green bottles".format(8)
     you'll come to see the pros and cons of each over time.
     """
-    pass
+    numberfield = []
+    for i in range(10):
+        number = []
+        for j in range(5):
+            number.append('(i' + str(i) + ', j' + str(j) + ')')
+        numberfield.append(number)
+    return numberfield
 
 
 def loops_6():
@@ -201,7 +214,16 @@ def loops_6():
     You can use a variable.
     TIP: look out for the starting condition.
     """
-    pass
+    numberfield = []
+    for i in range(10):
+        number = []
+        for j in range(10):
+            if j <= i:
+                number.append(str(j))
+            else:
+                break
+        numberfield.append(number)
+    return numberfield
 
 
 def loops_7():
@@ -225,7 +247,16 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    pass
+    numberfield = []
+    for i in range(5):
+        number = []
+        for j in range(9):
+            if j < 5 + i and j > 3 - i:
+                number.append('*')
+            else:
+                number.append(' ')
+        numberfield.append(number)
+    return numberfield
 
 
 def lp(some_kind_of_list, exercise_name):
